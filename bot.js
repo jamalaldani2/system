3d986ec364e1bc82d36f5a324800de12600ed513
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-08 17:32:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "Kx";
-
+client.on('message', message => {
+var prefix = "Kx"
+        if(message.content.startsWith(prefix + 'hypixel')) {
+            let args = message.content.split(' ').slice(1).join(' ');
+            if (!args) return message.channel.send("**رجأء ضع اسمك في ماين كرافت. ?**");
+            var link = (`https://hypixel.net/player/${args}`);
+            message.channel.send(link);
+        }
+    });
 
 
 
