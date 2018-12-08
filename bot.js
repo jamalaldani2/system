06e73a2068047c11c23346113e36e9f30e568715
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2018-12-08 17:43:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("Kxclose")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-       message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`+close\`. This will time out in 10 seconds and be cancelled.`)
+       message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`Kxclose\`. This will time out in 10 seconds and be cancelled.`)
            .then((m) => {
                message.channel.awaitMessages(response => response.content === 'Kxclose', {
                        max: 1,
